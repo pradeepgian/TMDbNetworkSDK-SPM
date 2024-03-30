262d1ba31abb9db08554adbc936218bad8a7cdaa
Pradeep Gianchandani <pradeep.gian@gmail.com>
2024-03-31 03:26:19 +0530

    Created Swift Package for TMDB Integration. Supports popular and latest movies retrieval.

--- Package.swift
+++ Package.swift
@@ -5,19 +5,19 @@ import PackageDescription
 
 let package = Package(
     name: "TMDbNetworkSDK",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "TMDbNetworkSDK",
-            targets: ["TMDbNetworkSDK"]),
+            targets: ["TMDbNetworkSDK"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "TMDbNetworkSDK"),
-        .testTarget(
-            name: "TMDbNetworkSDKTests",
-            dependencies: ["TMDbNetworkSDK"]),
+            .binaryTarget(
+                name: "TMDbNetworkSDK",
+                path: "./Sources/TMDbNetworkSDK.xcframework"
+            )
     ]
 )
---
+++ Sources/TMDbNetworkSDK.xcframework/ios-arm64_x86_64-simulator/TMDbNetworkSDK.framework/Headers/TMDbNetworkSDK.h
@@ -0,0 +1,18 @@
+//
+//  TMDbNetworkSDK.h
+//  TMDbNetworkSDK
+//
+//  Created by Pradeep Gianchandani on 31/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TMDbNetworkSDK.
+FOUNDATION_EXPORT double TMDbNetworkSDKVersionNumber;
+
+//! Project version string for TMDbNetworkSDK.
+FOUNDATION_EXPORT const unsigned char TMDbNetworkSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TMDbNetworkSDK/PublicHeader.h>
+
+
